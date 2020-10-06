589e808f753bc54cb1b5a7ecff6ccb91a0779b83
Flaymed <mvigg8@gmail.com>
2020-10-06 19:55:41 -0400

    Bug Fixes and Cleanup

--- src/main/java/com/podcrash/gamecore/kits/abilitytype/Interact.java
+++ src/main/java/com/podcrash/gamecore/kits/abilitytype/Interact.java
@@ -2,11 +2,17 @@ package com.podcrash.gamecore.kits.abilitytype;
 
 import com.podcrash.gamecore.kits.KitPlayer;
 import com.podcrash.gamecore.kits.KitPlayerManager;
-import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import java.util.List;
 
 public interface Interact extends IAbility {
+
+    ItemStack getItem();
+    void doAbility();
+    List<Action> getActions();
 
     @EventHandler
     default void interact(PlayerInteractEvent e) {
@@ -16,7 +22,8 @@ public interface Interact extends IAbility {
         KitPlayer kitPlayer = KitPlayerManager.getKitPlayerFromPlayer(e.getPlayer());
         if (kitPlayer == null) return;
         if (e.getItem() == null) return;
-        if (e.getItem().getType() != getItemType()) return;
+        if (e.getItem().getType() != getItem().getType()) return;
+        if (!getActions().contains(e.getAction())) return;
 
         if (this instanceof Cooldown) {
             if (!((Cooldown) this).hasCooldown()) return; //redundant? Assuming if this implements Cooldown it has a cooldown...
@@ -32,6 +39,4 @@ public interface Interact extends IAbility {
 
     }
 
-    Material getItemType();
-    void doAbility();
 }
